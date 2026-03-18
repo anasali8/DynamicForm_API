7fc639f4652c9e6fd52700f0aaad07ef8388e092
Anas Ali <anawlma11@gmail.com>
2026-03-18 23:55:57 +0200

    remove migrate

--- DynamicForm_API/Program.cs
+++ DynamicForm_API/Program.cs
@@ -82,12 +82,6 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
-    using var scope = app.Services.CreateScope();
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
 
 
 app.UseCors("AllowFrontend");
